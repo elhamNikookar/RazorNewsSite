18a9bb030f44d8f618599a94a9a953944117e32a
Elham <enikookar.ce@gmail.com>
2024-02-06 10:24:42 +0330

    initialize project

---
+++ Blazor.Data/Migrations/20240205090258_build database.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blazor.Data.Migrations
+{
+    public partial class builddatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
